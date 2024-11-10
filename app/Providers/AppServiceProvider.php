16781a4236f89930d98c789f60af327fddf9e23f
AndreyBozhko <andrii.bozhko@danads.se>
2024-11-10 16:57:40 +0200

    Fixed issues

--- app/Providers/AppServiceProvider.php
+++ app/Providers/AppServiceProvider.php
@@ -2,7 +2,9 @@
 
 namespace App\Providers;
 
+use App\Models\Product;
 use Illuminate\Auth\Notifications\ResetPassword;
+use Illuminate\Support\Facades\View;
 use Illuminate\Support\ServiceProvider;
 
 class AppServiceProvider extends ServiceProvider
@@ -23,5 +25,19 @@ class AppServiceProvider extends ServiceProvider
         ResetPassword::createUrlUsing(function (object $notifiable, string $token) {
             return config('app.frontend_url')."/password-reset/$token?email={$notifiable->getEmailForPasswordReset()}";
         });
+
+        $products = Product::query()
+            ->where('is_active', true)
+            ->where('is_top_seller', true)
+            ->whereHas('prices', function ($query) {
+                $query->where('type_id', function ($subQuery) {
+                    $subQuery->select('id')
+                        ->from('price_types')
+                        ->where('external_id', 'bb2a9a14-26f6-11ee-0a80-0f50000d072e');
+                })->where('price', '>', 0);
+            })
+            ->get();
+
+        View::share('products', $products);
     }
 }

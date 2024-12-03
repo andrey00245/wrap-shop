82dd76b23befde6acf47cba6371ebd42a5e36e48
sergejtolstikov <tosik19940@gmail.com>
2024-12-03 17:25:25 +0200

    favicon

--- resources/views/base/layouts/head.blade.php
+++ resources/views/base/layouts/head.blade.php
@@ -11,6 +11,22 @@
   <link rel="stylesheet" href="{{asset('assets/css/font-awesome.css')}}" type="text/css" media="screen">
   <link rel="stylesheet" href="{{asset('assets/css/swiper-bundle.min.css')}}" media="screen">
 
+  <link rel="icon" type="image/png" sizes="32x32" href="{{asset('favicon-32x32.png')}}" type="image/x-icon">
+  <link rel="icon" href="{{asset('favicon-32x32.png')}}" type="image/x-icon">
+  <link rel="icon" href="{{asset('favicon-32x32.png')}}" type="image/x-icon">
+  <link rel="icon" href="{{asset('favicon-32x32.png')}}" type="image/x-icon">
+  <link rel="icon" href="{{asset('favicon-32x32.png')}}" type="image/x-icon">
+
+
+  <link rel="icon" href="{{asset('assets/favicon/favicon.ico')}}" type="image/x-icon">
+  <link rel="icon" sizes="16x16" href="{{asset('assets/favicon/favicon-16x16.png')}}">
+  <link rel="icon" sizes="32x32" href="{{asset('assets/favicon/favicon-32x32.png')}}">
+  <meta name="theme-color" content="#ffffff">
+
+  <link rel="apple-touch-icon" href="{{asset('assets/favicon/apple-touch-icon.png')}}">
+  <link rel="icon" type="image/png" sizes="192x192" href="{{asset('assets/favicon/android-chrome-96x96.png')}}">
+
+
   @stack('styles')
 
   <link rel="alternate" hreflang="uk-ua" href="{{LaravelLocalization::getLocalizedURL('uk')}}">

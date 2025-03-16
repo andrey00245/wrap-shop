ce6f29436c38b7f4f8b585535f3197d5abd3b3ae
sergejtolstikov <tosik19940@gmail.com>
2025-03-16 16:41:04 +0200

    add white theme and some style fixes

--- bootstrap/app.php
+++ bootstrap/app.php
@@ -36,6 +36,7 @@ return Application::configure(basePath: dirname(__DIR__))
         'localeSessionRedirect'   => \Mcamara\LaravelLocalization\Middleware\LocaleSessionRedirect::class,
         'localeCookieRedirect'    => \Mcamara\LaravelLocalization\Middleware\LocaleCookieRedirect::class,
         'localeViewPath'          => \Mcamara\LaravelLocalization\Middleware\LaravelLocalizationViewPath::class,
+        'themeMiddleware'          => \App\Http\Middleware\ThemeMiddleware::class,
         'redirect_if_not_authenticated' => RedirectIfNotAuthenticated::class,
       ]);
 
--- resources/views/base/layouts/head.blade.php
+++ resources/views/base/layouts/head.blade.php
@@ -1,37 +1,41 @@
 <head>
-  <meta charset="UTF-8">
-  <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no, user-scalable=no">
-  <meta http-equiv="X-UA-Compatible" content="IE=edge">
-  <meta name="locale" content="{{ app()->getLocale() }}"/>
-  <title>@yield('title', 'Default')</title>
-  <meta name="description" content="@yield('description', 'Default')">
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no, user-scalable=no">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="locale" content="{{ app()->getLocale() }}"/>
+    <title>@yield('title', 'Default')</title>
+    <meta name="description" content="@yield('description', 'Default')">
 
-  <link href="{{mix('build/css/style-dark.css')}}" rel="stylesheet" type="text/css">
-  <link rel="stylesheet" type="text/css" href="{{mix('build/css/social-login.css')}}">
-  <link rel="stylesheet" href="{{asset('assets/css/fonts.css')}}" type="text/css" media="screen">
-  <link rel="stylesheet" href="{{asset('assets/css/font-awesome.css')}}" type="text/css" media="screen">
-  <link rel="stylesheet" href="{{asset('assets/css/swiper-bundle.min.css')}}" media="screen">
+    @if($theme === 'dark')
+        <link href="{{mix('build/css/style-dark.css')}}" rel="stylesheet" type="text/css">
+    @else
+        <link href="{{mix('build/css/style-light.css')}}" rel="stylesheet" type="text/css">
+    @endif
+    <link rel="stylesheet" type="text/css" href="{{mix('build/css/social-login.css')}}">
+    <link rel="stylesheet" href="{{asset('assets/css/fonts.css')}}" type="text/css" media="screen">
+    <link rel="stylesheet" href="{{asset('assets/css/font-awesome.css')}}" type="text/css" media="screen">
+    <link rel="stylesheet" href="{{asset('assets/css/swiper-bundle.min.css')}}" media="screen">
 
-  <link rel="icon" href="{{asset('assets/favicon/favicon.ico')}}" type="image/x-icon">
-  <link rel="icon" sizes="16x16" href="{{asset('assets/favicon/favicon-16x16.png')}}">
-  <link rel="icon" sizes="32x32" href="{{asset('assets/favicon/favicon-32x32.png')}}">
-  <meta name="theme-color" content="#ffffff">
+    <link rel="icon" href="{{asset('assets/favicon/favicon.ico')}}" type="image/x-icon">
+    <link rel="icon" sizes="16x16" href="{{asset('assets/favicon/favicon-16x16.png')}}">
+    <link rel="icon" sizes="32x32" href="{{asset('assets/favicon/favicon-32x32.png')}}">
+    <meta name="theme-color" content="#ffffff">
 
-  <link rel="apple-touch-icon" href="{{asset('assets/favicon/apple-touch-icon.png')}}">
-  <link rel="icon" type="image/png" sizes="192x192" href="{{asset('assets/favicon/android-chrome-96x96.png')}}">
+    <link rel="apple-touch-icon" href="{{asset('assets/favicon/apple-touch-icon.png')}}">
+    <link rel="icon" type="image/png" sizes="192x192" href="{{asset('assets/favicon/android-chrome-96x96.png')}}">
 
-  @stack('styles')
+    @stack('styles')
 
-  <link rel="alternate" hreflang="uk-ua" href="{{LaravelLocalization::getLocalizedURL('uk')}}">
-  <link rel="alternate" hreflang="x-default" href="{{LaravelLocalization::getLocalizedURL('uk')}}">
-  <link rel="alternate" hreflang="ru-ua" href="{{LaravelLocalization::getLocalizedURL('ru')}}">
-  <link rel="alternate" hreflang="en-gb" href="{{LaravelLocalization::getLocalizedURL('en')}}">
+    <link rel="alternate" hreflang="uk-ua" href="{{LaravelLocalization::getLocalizedURL('uk')}}">
+    <link rel="alternate" hreflang="x-default" href="{{LaravelLocalization::getLocalizedURL('uk')}}">
+    <link rel="alternate" hreflang="ru-ua" href="{{LaravelLocalization::getLocalizedURL('ru')}}">
+    <link rel="alternate" hreflang="en-gb" href="{{LaravelLocalization::getLocalizedURL('en')}}">
 
-  <style>
+    <style>
 
-    header {
-      min-height: 84px;
-    }
+      header {
+        min-height: 84px;
+      }
 
-  </style>
+    </style>
 </head>

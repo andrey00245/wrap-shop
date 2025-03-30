5a27ad0b598a3a8dbb60ae35aa6ea8a740ae67ea
sergejtolstikov <tosik19940@gmail.com>
2025-03-30 13:52:46 +0300

    intlTelinput lib

---
+++ public/third-party/intlTelInput/js/i18n/uk/interface.js
@@ -0,0 +1,13 @@
+const interfaceTranslations = {
+  selectedCountryAriaLabel: "Обрана країна",
+  noCountrySelected: "Країна не обрана",
+  countryListAriaLabel: "Список країн",
+  searchPlaceholder: "Пошук",
+  zeroSearchResults: "результатів не знайдено",
+  oneSearchResult: "знайдено 1 результат",
+  multipleSearchResults: "Знайдено ${count} результатів",
+  // additional countries (not supported by country-list library)
+  ac: "Остров Вознесения",
+  xk: "Косово"
+};
+export default interfaceTranslations;

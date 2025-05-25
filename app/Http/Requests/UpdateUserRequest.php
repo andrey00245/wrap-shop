a1df1298d93ac62840f31241fe4efc0e3a982d6f
sergejtolstikov <tosik19940@gmail.com>
2025-05-25 18:29:20 +0300

    some fixes

--- app/Http/Requests/UpdateUserRequest.php
+++ app/Http/Requests/UpdateUserRequest.php
@@ -24,33 +24,33 @@ class UpdateUserRequest extends FormRequest
     public function rules(): array
     {
         return [
-          'name' => ['required', 'string','min:3', 'max:12'],
-          'last_name' => ['string', 'min:3', 'max:15'],
-          'email' => ['required', 'string', 'email', 'min:3', 'max:255', Rule::unique(User::class)->ignore($this->user()->id)],
-          'phone' => ['required', 'string','regex:/^([0-9\s\-\+\(\)]*)$/', 'min:10', 'max:255', Rule::unique(User::class)->ignore($this->user()->id)],
+            'name' => ['required', 'string', 'min:3', 'max:12'],
+            'last_name' => ['string', 'min:3', 'max:15'],
+            'email' => ['required', 'string', 'email', 'min:3', 'max:255', Rule::unique(User::class)->ignore($this->user()->id)],
+            'phone' => ['required', 'string', 'regex:/^([0-9\s\-\+\(\)]*)$/', 'min:10', 'max:255', Rule::unique(User::class)->ignore($this->user()->id)],
         ];
     }
     public function messages()
     {
-      return [
-        'name.required' => __('validation.name.required'),
-        'name.min' => __('validation.name.required'),
-        'name.max' => __('validation.name.required'),
-        'name.string' => __('validation.name.required'),
-        'last_name.min' => __('validation.last_name.required'),
-        'last_name.max' => __('validation.last_name.required'),
-        'last_name.string' => __('validation.last_name.required'),
-        'email.required' => __('validation.email.email'),
-        'email.string' => __('validation.email.email'),
-        'email.email' => __('validation.email.email'),
-        'email.unique' => __('validation.email.unique'),
-        'email.min' => __('validation.email.email'),
-        'email.max' => __('validation.email.email'),
-        'phone.required' => __('validation.phone.required'),
-        'phone.unique' => __('validation.phone.regex'),
-        'phone.regex' => __('validation.phone.regex'),
-        'phone.min' => __('validation.phone.regex'),
-        'phone.max' => __('validation.phone.regex'),
-      ];
+        return [
+            'name.required' => __('validation.name.required'),
+            'name.min' => __('validation.name.required'),
+            'name.max' => __('validation.name.required'),
+            'name.string' => __('validation.name.required'),
+            'last_name.min' => __('validation.last_name.required'),
+            'last_name.max' => __('validation.last_name.required'),
+            'last_name.string' => __('validation.last_name.required'),
+            'email.required' => __('validation.email.email'),
+            'email.string' => __('validation.email.email'),
+            'email.email' => __('validation.email.email'),
+            'email.unique' => __('validation.email.unique'),
+            'email.min' => __('validation.email.email'),
+            'email.max' => __('validation.email.email'),
+            'phone.required' => __('validation.phone.required'),
+            'phone.unique' => __('validation.unique', ['attribute' => __('personal-account.personal-data.telephone')]),
+            'phone.regex' => __('validation.phone.regex'),
+            'phone.min' => __('validation.min.string', ['attribute' => __('personal-account.personal-data.telephone')]),
+            'phone.max' => __('validation.max.string', ['attribute' => __('personal-account.personal-data.telephone')]),
+        ];
     }
 }

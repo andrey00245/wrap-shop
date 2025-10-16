288d19bf783d97e8209e122e9f20266a7b053106
AndreyBozhko <andrii.bozhko@danads.se>
2025-10-16 16:12:22 +0300

    added new feature

--- app/Services/MoySkladSyncService.php
+++ app/Services/MoySkladSyncService.php
@@ -388,7 +388,6 @@ class MoySkladSyncService
             'moment'       => now()->format('Y-m-d H:i:s'),
         ];
 
-            dd($payload);
         $comment = '';
 
         if (in_array($order->shipping_method, ['novaposhta', 'novaposhta_doors', 'my_addresses'])) {

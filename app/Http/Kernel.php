e207849333574100415d7d4a2100898cf80e7213
andrii.bozhko <andrii.bozhko@sigma.software>
2026-04-11 18:21:13 +0300

    Updated logic

--- app/Http/Kernel.php
+++ app/Http/Kernel.php
@@ -3,7 +3,6 @@
 namespace App\Http;
 
 use App\Http\Middleware\CartMiddleware;
-use App\Http\Middleware\RedirectMiddleware;
 use Illuminate\Foundation\Http\Kernel as HttpKernel;
 
 class Kernel extends HttpKernel
@@ -17,7 +16,6 @@ class Kernel extends HttpKernel
     protected $middleware = [
         \Illuminate\Http\Middleware\HandleCors::class,
         \Illuminate\Foundation\Http\Middleware\ValidatePostSize::class,
-        RedirectMiddleware::class,
         \Illuminate\Session\Middleware\StartSession::class,
         \Illuminate\View\Middleware\ShareErrorsFromSession::class,
         \Illuminate\Routing\Middleware\SubstituteBindings::class,
